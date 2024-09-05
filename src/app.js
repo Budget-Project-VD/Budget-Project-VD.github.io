ecb3957090a9ce0e7ed32d4488ac43922a62d452
Velin Dankov <velin80@web.de>
2024-09-05 09:47:49 +0300

    adding eslint configurator - and redactering app.js and index.html

--- src/app.js
+++ src/app.js
@@ -1,19 +1,7 @@
-import  page  from "../lib/page/page.mjs";
+import page from "../lib/page/page.mjs";
 import { html, render } from "../lib/lit-html/lit-html.js";
 
+const temp = () => html` <h1>This is my test page</h1> `;
 
-
-
-const temp = () => html `
-  <h1>This is my test page</h1>
-  `
-
-
-function showHome() {
-  render(temp())
-  
-}
-
-page(render(temp(), document.querySelector('main')));
+page(render(temp(), document.querySelector("main")));
 page.start();
-
